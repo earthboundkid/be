dbcb7ae0f93bb3e67b07d96f6f130e520da31579
Carl Johnson <me@carlmjohnson.net>
2023-05-24 12:13:30 -0400

    Relaxed: More relaxing

--- relaxed.go
+++ relaxed.go
@@ -4,7 +4,9 @@ import (
 	"testing"
 )
 
-// Relaxed returns a testing.TB which replaces calls to t.Fatalf with calls to t.Errorf.
+// Relaxed returns a testing.TB
+// which replaces calls to t.FailNow, t.Fatal, and t.Fatalf
+// with calls to t.Fail, t.Error, and t.Errorf respectively.
 func Relaxed(t testing.TB) testing.TB {
 	return relaxed{t}
 }
@@ -13,7 +15,17 @@ type relaxed struct {
 	testing.TB
 }
 
+func (r relaxed) Fatal(args ...any) {
+	r.Helper()
+	r.Error(args...)
+}
+
 func (r relaxed) Fatalf(format string, args ...any) {
 	r.Helper()
 	r.Errorf(format, args...)
+}
+
+func (r relaxed) FailNow() {
+	r.Helper()
+	r.Fail()
 }
