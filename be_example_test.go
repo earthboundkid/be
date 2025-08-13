1038b2742dc0dfc1e1cca4d2488588e77115989a
Carlana <me@carlmjohnson.net>
2025-08-13 12:49:29 -0400

    Add EqualLength, AtLeastLength (#23)
    * Add EqualLength, AtLeastLength
    
    * Upgrade Go
    
    * Add coverage

--- be_example_test.go
+++ be_example_test.go
@@ -2,6 +2,7 @@ package be_test
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/carlmjohnson/be"
 )
@@ -34,6 +35,14 @@ func Example() {
 	be.NotIn(t, "\x01", []byte("\a\b\x00\r\t")) // good
 	be.NotIn(t, "\x00", []byte("\a\b\x00\r\t")) // bad
 
+	seq := strings.FieldsSeq("1 2 3 4")
+	be.EqualLength(t, 4, seq)     // good
+	be.EqualLength(t, 1, seq)     // bad
+	be.AtLeastLength(t, 1, seq)   // good
+	be.AtLeastLength(t, 5, seq)   // bad
+	be.AtLeastLength(t, 3, "123") // good
+	be.AtLeastLength(t, 4, "123") // bad
+
 	// Output:
 	// want: hello; got: world
 	// got: goodbye
@@ -42,4 +51,7 @@ func Example() {
 	// got: (O_o)
 	// "World" not in "hello, world"
 	// "\x00" in "\a\b\x00\r\t"
+	// want len(seq) == 1; got at least 2
+	// want len(seq) >= 5; got 4
+	// want len(seq) >= 4; got 3
 }
