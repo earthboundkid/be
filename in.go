d2874f2afc4aee75e1b974fc7feb8d7a675a57e8
Carlana <mail@carlana.net>
2025-09-26 16:17:24 -0400

    Deprecate In, NotIn

--- in.go
+++ in.go
@@ -1,35 +1,24 @@
 package be
 
 import (
-	"bytes"
-	"reflect"
-	"strings"
+	"regexp"
 	"testing"
 )
 
 // In calls t.Fatalf if needle is not contained in the string or []byte haystack.
+//
+// Deprecated: Use Match(t, regexp.QuoteMeta(needle), haystack).
+//
+//go:fix inline
 func In[byteseq ~string | ~[]byte](t testing.TB, needle string, haystack byteseq) {
-	t.Helper()
-	if !in(needle, haystack) {
-		t.Fatalf("%q not in %q", needle, haystack)
-	}
+	Match(t, regexp.QuoteMeta(needle), haystack)
 }
 
 // NotIn calls t.Fatalf if needle is contained in the string or []byte haystack.
+//
+// Deprecated: Use NoMatch(t, regexp.QuoteMeta(needle), haystack).
+//
+//go:fix inline
 func NotIn[byteseq ~string | ~[]byte](t testing.TB, needle string, haystack byteseq) {
-	t.Helper()
-	if in(needle, haystack) {
-		t.Fatalf("%q in %q", needle, haystack)
-	}
-}
-
-func in[byteseq ~string | ~[]byte](needle string, haystack byteseq) bool {
-	rv := reflect.ValueOf(haystack)
-	switch rv.Kind() {
-	case reflect.String:
-		return strings.Contains(rv.String(), needle)
-	case reflect.Slice:
-		return bytes.Contains(rv.Bytes(), []byte(needle))
-	}
-	panic("unreachable")
+	NoMatch(t, regexp.QuoteMeta(needle), haystack)
 }
